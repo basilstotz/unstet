84649f9e46f682c6a3ffc583cd605e3e2a03cfd5
Basil Stotz <stotz.basil@opinsys.fi>
2023-05-09 20:09:52 +0200

    renamed

--- bin/build.sh
+++ bin/build.sh
@@ -5,10 +5,3 @@ find . -name "unstet_*_all.deb" -exec rm \{\} \;
 sed  unstet/DEBIAN/control.template -e "s/%%version%%/$(date +%s)/" > unstet/DEBIAN/control
 dpkg-deb -b unstet .
 rm unstet/DEBIAN/control
-
-#osctools
-find . -name "osctools_*_all.deb" -exec rm \{\} \;
-sed  osctools/DEBIAN/control.template -e "s/%%version%%/$(date +%s)/" > osctools/DEBIAN/control
-dpkg-deb -b osctools .
-rm osctools/DEBIAN/control
-
