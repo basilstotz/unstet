46454964ba97156ee03b52aab69bf9857466d0f1
Basil Stotz <stotz.basil@opinsys.fi>
2023-08-21 13:26:08 +0200

    fix setup-screensaver

--- unstet/usr/local/bin/unstet-setup-user-autostart.sh
+++ unstet/usr/local/bin/unstet-setup-user-autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if test -d /usr/share/doc/unstet/autostart/; then
+if test -d /usr/share/doc/unstet/raspi-autostart/; then
    mkdir -p $HOME/.config/autostart
-   cp /usr/share/doc/unstet/raspi-autostart/* $HOME.config/autostart/
+   cp /usr/share/doc/unstet/raspi-autostart/* $HOME/.config/autostart/
 fi
